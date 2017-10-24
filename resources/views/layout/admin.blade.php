570a38bdee7ae6e4bb93cd2d38aa18ae0bb92600
Programador3 <roberth1136@gmail.com>
2017-10-24 16:13:51 -0500

    crear link cronograma

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -119,18 +119,10 @@
                     </ul>
                 </li>
                 <li>
-                    <a href="#"><i class="fa fa-files-o"></i> <span class="nav-label">Other Pages</span><span class="fa arrow"></span></a>
+                    <a href="#"><i class="fa fa-files-o"></i> <span class="nav-label">Cronograma y Plazos</span><span class="fa arrow"></span></a>
                     <ul class="nav nav-second-level collapse">
-                        <li><a href="search_results.html">Search results</a></li>
-                        <li><a href="lockscreen.html">Lockscreen</a></li>
-                        <li><a href="invoice.html">Invoice</a></li>
-                        <li><a href="login.html">Login</a></li>
-                        <li><a href="login_two_columns.html">Login v.2</a></li>
-                        <li><a href="forgot_password.html">Forget password</a></li>
-                        <li><a href="register.html">Register</a></li>
-                        <li><a href="404.html">404 Page</a></li>
-                        <li><a href="500.html">500 Page</a></li>
-                        <li><a href="empty_page.html">Empty page</a></li>
+                        <li><a href="search_results.html">Crear cronograma</a></li>
+                        <li><a href="lockscreen.html">Listar Cronogamas</a></li>
                     </ul>
                 </li>
                 <li>
