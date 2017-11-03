69a214a8f7636c3ee339d2dea5daf1006c7a2eff
Programador3 <roberth1136@gmail.com>
2017-11-03 12:58:35 -0500

    fakes para proyecto

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,9 @@ class DatabaseSeeder extends Seeder
     {
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
+
         $this->call([
+
             EtapaTableSeeder::class,
             RolTableSeeder::class,
             UsuarioTableSeeder::class,
@@ -23,12 +25,32 @@ class DatabaseSeeder extends Seeder
             NormativaCSeeder::class,
             InstitucionTableSeeder::class,
             InfoSoftwareSeeder::class,
-            MacroprocesoSeeder::class,
-            ProcesomaSeeder::class,
-            SubprocesoSeeder::class,
-            ProcedimientospSeeder::class,
-            ActividadesSeeder::class
+           // MacroprocesoSeeder::class,
+            //ProcesomaSeeder::class,
+            //SubprocesoSeeder::class,
+            //ProcedimientospSeeder::class,
+         //   ActividadesSeeder::class
             ]);
+
+        factory('App\Models\Plan', 2)->create();
+        factory('App\Models\Auditoria', 10)->create();
+        factory('App\Models\ObjetivoGeneral', 4)->create();
+        factory('App\Models\Macroproceso', 10)->create();
+        factory('App\Models\ObjetivoEspecifico', 10)->create();
+        factory('App\Models\Procesoma', 10)->create();
+        factory('App\Models\Subproceso', 15)->create();
+        factory('App\Models\Procedimientosp', 15)->create();
+        factory('App\Models\Actividad', 15)->create();
+        factory('App\Models\Usuariorol', 15)->create();
+        factory('App\Models\Procedimiento', 15)->create();
+        factory('App\Models\Informe', 10)->create();
+        factory('App\Models\Cronograma', 10)->create();
+        factory('App\Models\NormativaMarcoproceso', 15)->create();
+        factory('App\Models\Normativac', 10)->create();
+
+
+
+
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
