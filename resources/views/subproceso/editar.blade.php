95b261cfa375b2ccca91e99de9b2e1d69a301611
Cesar Herbozo <cesarhm1687@gmail.com>
2017-11-08 18:07:40 -0500

    cambiando frontend A MACROCESOS Y EL RESTO

--- resources/views/subproceso/editar.blade.php
+++ resources/views/subproceso/editar.blade.php
@@ -3,37 +3,25 @@
     <div class="row">
         <div class="col-lg-12">
             <div class="ibox float-e-margins">
-                <div class="ibox-title">
-                    <h5>EDITAR SUBPROCESO</h5>
-                    <div class="ibox-tools">
-                        <a class="collapse-link">
-                            <i class="fa fa-chevron-up"></i>
-                        </a>
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                            <i class="fa fa-wrench"></i>
-                        </a>
-                        <ul class="dropdown-menu dropdown-user">
-                            <li><a href="#">Config option 1</a>
-                            </li>
-                            <li><a href="#">Config option 2</a>
-                            </li>
-                        </ul>
-                        <a class="close-link">
-                            <i class="fa fa-times"></i>
-                        </a>
-                    </div>
-                </div>
                 <div class="ibox-content">
+                    <div class="panel panel-success">
+                        <div class="panel-heading">
+                            EDITAR SUBPROCESO
+                        </div>
+                        <div class="panel-body">
+                            {!! Form::open(['method' => 'POST', 'route' => 'subproceso.actualizar']) !!}
+                            <input type="hidden" value="{{$subproceso->codProMA}}" name="codProMA">
+                            <input type="hidden" value="{{$subproceso->codSubPro}}" name="codSubPro">
+                            {!! Field::text('nombre', $subproceso->nombre, ['label' => 'Nombre']) !!}
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-success btn-outline" value=""><i class="fa fa-save"></i> ACTUALIZAR</button>
+                                <a href="{{URL::to('procesoma/mostrar')}}/{{$subproceso->codSubPro}}"  class="btn btn-danger btn-outline">CANCELAR</a>
+                            </div>
+                            {!! Form::close() !!}
+                        </div>
 
-                    {!! Form::open(['method' => 'POST', 'route' => 'subproceso.actualizar']) !!}
-                    <input type="hidden" value="{{$subproceso->codProMA}}" name="codProMA">
-                    <input type="hidden" value="{{$subproceso->codSubPro}}" name="codSubPro">
-                    {!! Field::text('nombre', $subproceso->nombre, ['label' => 'Nombre']) !!}
-                    <div class="form-group">
-                        <input type="submit" class="btn btn-primary btn-outline" value="ACTUALIZAR">
-                        <a href="{{URL::to('procesoma/mostrar')}}/{{$subproceso->codSubPro}}"  class="btn btn-danger btn-outline">CANCELAR</a>
                     </div>
-                    {!! Form::close() !!}
+
                 </div>
             </div>
         </div>
