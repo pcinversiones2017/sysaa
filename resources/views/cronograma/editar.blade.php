22710f0384a6f43cdec0815f59e505b2aa69d77c
Gilmar Moreno <gilmarmoreno1993@gmail.com>
2017-12-07 15:03:56 -0500

    Se realizo cambios

--- resources/views/cronograma/editar.blade.php
+++ resources/views/cronograma/editar.blade.php
@@ -10,7 +10,7 @@
 
                     <div class="panel panel-success">
                         <div class="panel-heading">
-                            EDITAR CRONOGRAMA <small>CRONOGRAMA Y PLAZOS DE ENTREGA DE DOCUMENTOS (*)</small>
+                            EDITAR <small>CRONOGRAMA Y PLAZOS DE ENTREGA DE DOCUMENTOS (*)</small>
                         </div>
                         <div class="panel-body">
                             <div class="">
